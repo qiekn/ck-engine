9b8a2b5faaf435825f80f649d9a59469808d7b7d
qiekn <qieknxxx@gmail.com>
2025-09-11 15:38:07 +0800

    fix: ::ck -> logging macros to use global namespace qualifier

--- src/ck/log.h
+++ src/ck/log.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+
 #include "spdlog/spdlog.h"
 
 namespace ck {
@@ -20,15 +21,15 @@ private:
 }  // namespace ck
 
 // clang-format off
-#define CK_ENGINE_TRACE(...) ck::Log::GetEngineLogger()->trace(__VA_ARGS__)
-#define CK_ENGINE_INFO(...)  ck::Log::GetEngineLogger()->info(__VA_ARGS__)
-#define CK_ENGINE_WARN(...)  ck::Log::GetEngineLogger()->warn(__VA_ARGS__)
-#define CK_ENGINE_ERROR(...) ck::Log::GetEngineLogger()->error(__VA_ARGS__)
-#define CK_ENGINE_FATAL(...) ck::Log::GetEngineLogger()->fatal(__VA_ARGS__)
+#define CK_ENGINE_TRACE(...) ::ck::Log::GetEngineLogger()->trace(__VA_ARGS__)
+#define CK_ENGINE_INFO(...)  ::ck::Log::GetEngineLogger()->info(__VA_ARGS__)
+#define CK_ENGINE_WARN(...)  ::ck::Log::GetEngineLogger()->warn(__VA_ARGS__)
+#define CK_ENGINE_ERROR(...) ::ck::Log::GetEngineLogger()->error(__VA_ARGS__)
+#define CK_ENGINE_FATAL(...) ::ck::Log::GetEngineLogger()->fatal(__VA_ARGS__)
 
-#define CK_CLIENT_TRACE(...) ck::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define CK_CLIENT_INFO(...)  ck::Log::GetClientLogger()->info(__VA_ARGS__)
-#define CK_CLIENT_WARN(...)  ck::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define CK_CLIENT_ERROR(...) ck::Log::GetClientLogger()->error(__VA_ARGS__)
-#define CK_CLIENT_FATAL(...) ck::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define CK_CLIENT_TRACE(...) ::ck::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define CK_CLIENT_INFO(...)  ::ck::Log::GetClientLogger()->info(__VA_ARGS__)
+#define CK_CLIENT_WARN(...)  ::ck::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define CK_CLIENT_ERROR(...) ::ck::Log::GetClientLogger()->error(__VA_ARGS__)
+#define CK_CLIENT_FATAL(...) ::ck::Log::GetClientLogger()->fatal(__VA_ARGS__)
 // clang-format on

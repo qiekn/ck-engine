5e19e20558b6b46e4df55bc118c016bbe85840a2
qiekn <qiekndev@gmail.com>
2025-10-05 04:57:25 +0800

    chore: imgui remove demo window

--- src/ck/imgui/imgui_layer.cpp
+++ src/ck/imgui/imgui_layer.cpp
@@ -56,8 +56,8 @@ void ImGuiLayer::OnDetach() {
 }
 
 void ImGuiLayer::OnImGuiRender() {
-  static bool show = true;
-  ImGui::ShowDemoWindow(&show);
+  // static bool show = false;
+  // ImGui::ShowDemoWindow(&show);
 }
 
 void ImGuiLayer::Begin() {

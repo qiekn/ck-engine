03e9348378c974c374aa7603f8a79a02a0f28d16
qiekn <qieknxxx@gmail.com>
2025-11-21 19:36:18 +0800

    update: unix run script

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-BUILD_DIR="build"
+DIR="build"
 
 if [ "$1" = "debug" ]; then
   lldb ./build/sandbox
 else
-  make -C "$BUILD_DIR" && make -j$(nproc) -C "$BUILD_DIR" && ./"$BUILD_DIR"/sandbox
+  cmake --build ${DIR} -j$(nproc) && ./${DIR}/sandbox
 fi
 
 # vim: ft=sh ts=2 sw=2 et

2a7af37b22e8ca336b87f491f3f16af32c22da3e
qiekn <qiekndev@gmail.com>
2025-12-04 00:52:45 +0800

    refactor: enum class use uint8_t and format macros

--- src/engine/events/event.h
+++ src/engine/events/event.h
@@ -4,7 +4,7 @@
 
 namespace ck {
 // clang-format off
-enum class EventType {
+enum class EventType : uint8_t {
   None = 0,
 
   WindowClose,
@@ -29,7 +29,7 @@ enum class EventType {
 // 这个枚举是用来做 Filter 的
 // 这是使用普通枚举类型, 目的是方便 OR / AND 操作
 // 可以隐式转换为 int, 而 enum class 需要显式 cast
-enum EventCategory {
+enum EventCategory: uint8_t {
   None = 0,
   EventCategoryApplication = BIT(0), // 0b1
   EventCategoryInput =       BIT(1), // 0b10
@@ -37,16 +37,25 @@ enum EventCategory {
   EventCategoryMouse =       BIT(3),
   EventCategoryMouseButton = BIT(4)
 };
+
 // clang-format on
 
 // # 用来把参数转为字符串字面量
-#define EVENT_CLASS_TYPE(type)                                                \
-  static EventType GetStaticType() { return EventType::type; }                \
-  virtual EventType GetEventType() const override { return GetStaticType(); } \
-  virtual const char* GetName() const override { return #type; }
+#define EVENT_CLASS_TYPE(type)                      \
+  static EventType GetStaticType() {                \
+    return EventType::type;                         \
+  }                                                 \
+  virtual EventType GetEventType() const override { \
+    return GetStaticType();                         \
+  }                                                 \
+  virtual const char* GetName() const override {    \
+    return #type;                                   \
+  }
 
-#define EVENT_CLASS_CATEGORY(category) \
-  virtual int GetCategoryFlags() const override { return category; }
+#define EVENT_CLASS_CATEGORY(category)            \
+  virtual int GetCategoryFlags() const override { \
+    return category;                              \
+  }
 
 #define CK_BIND_EVENT(fn)                                   \
   [this](auto&&... args) -> decltype(auto) {                \
@@ -60,12 +69,15 @@ public:
   virtual auto GetEventType() const -> EventType = 0;
   virtual const char* GetName() const = 0;
   virtual int GetCategoryFlags() const = 0;
+
   virtual std::string ToString() const { return GetName(); }
 
   inline virtual bool IsInCategorpy(EventCategory category) {
     return GetCategoryFlags() & category;
   }
+
   inline bool IsHandled() const { return handled_; }
+
   inline bool& GetIsHandled() { return handled_; }
 
 protected:

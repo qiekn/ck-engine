e732ea93b0e9912ee395534d40533a74b7cb3aa1
qiekn <qiekndev@gmail.com>
2025-12-06 02:41:11 +0800

    feat: impl operator uint32_t/==/!= at Entity class

--- src/engine/scene/entity.h
+++ src/engine/scene/entity.h
@@ -40,6 +40,14 @@ public:
 
   explicit operator bool() const { return entity_handle_ != entt::null; }
 
+  explicit operator uint32_t() const { return (uint32_t)entity_handle_; }
+
+  bool operator==(const Entity& other) const {
+    return entity_handle_ == other.entity_handle_ && scene_ == other.scene_;
+  }
+
+  bool operator!=(const Entity& other) const { return !(*this == other); }
+
 private:
   entt::entity entity_handle_{entt::null};
   Scene* scene_{nullptr};

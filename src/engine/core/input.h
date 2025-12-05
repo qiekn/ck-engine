685efd50e001e528b9726fc0df852fa718dbb9f8
qiekn <qiekndev@gmail.com>
2025-12-05 22:18:14 +0800

    refactor: use glm::vec2 instead of std::pair<float, float> in func GetMousePos()

--- src/engine/core/input.h
+++ src/engine/core/input.h
@@ -2,13 +2,14 @@
 
 #include "events/key_codes.h"
 #include "events/mouse_codes.h"
+#include "glm/ext/vector_float2.hpp"
 
 namespace ck {
 class Input {
 public:
   static bool IsKeyPressed(KeyCode key);
   static bool IsMouseButtonPressed(MouseCode button);
-  static std::pair<float, float> GetMousePos();
+  static glm::vec2 GetMousePos();
   static float GetMouseX();
   static float GetMouseY();
 };
--- src/engine/platform/windows_input.cpp
+++ src/engine/platform/windows_input.cpp
@@ -5,19 +5,19 @@
 
 namespace ck {
 
-bool Input::IsKeyPressed(KeyCode key) {
-  auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
+bool Input::IsKeyPressed(const KeyCode key) {
+  auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
   auto state = glfwGetKey(window, static_cast<int32_t>(key));
   return state == GLFW_PRESS || state == GLFW_REPEAT;
 }
 
-bool Input::IsMouseButtonPressed(MouseCode button) {
-  auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
+bool Input::IsMouseButtonPressed(const MouseCode button) {
+  auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
   auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
   return state == GLFW_PRESS;
 }
 
-std::pair<float, float> Input::GetMousePos() {
+glm::vec2 Input::GetMousePos() {
   auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
   double x, y;
   glfwGetCursorPos(window, &x, &y);
@@ -25,13 +25,11 @@ std::pair<float, float> Input::GetMousePos() {
 }
 
 float Input::GetMouseX() {
-  auto [x, y] = GetMousePos();
-  return x;
+  return GetMousePos().x;
 }
 
 float Input::GetMouseY() {
-  auto [x, y] = GetMousePos();
-  return y;
+  return GetMousePos().y;
 }
 
 }  // namespace ck
